a4c9b5be0e24cc7333394689b2a63a70a0f8d638
Brad Jolicoeur <brad@jolicoeurs.net>
2021-09-28 17:57:40 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # martin-banking-eventsource
-This is my take on the Marten Bank Sample
+
+This is my take on the Marten Bank Sample.  Most of the code for this came from the following repo:
+
+https://github.com/khalidabuhakmeh/marten-bank-sample
+
+
+The `docker-compose.yml` will spin up an instance of PostgreSQL that this example will target.  
---
+++ src/marten-banking-eventsource/Models/Events/AccountCredited.cs
@@ -0,0 +1,24 @@
+using Accounting.Projections;
+
+namespace Accounting.Events
+{
+    public class AccountCredited : Transaction
+    {
+      public override void Apply(Account account) {
+          account.Balance += Amount;
+      }
+
+      public AccountDebited ToDebit() {
+          return new AccountDebited {
+              Amount = Amount,
+              To = From,
+              From = To,
+              Description = Description
+          };
+      }
+
+      public override string ToString() {
+          return $"{Time} Credited {Amount.ToString("C")} From {From}";
+      }
+    }
+}
